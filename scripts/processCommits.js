247df591415431305f29ae5aeb6ef7b3300a6427
ognerezov <ognerezov@gmail.com>
2024-09-25 11:55:58 +0200

    build log file

--- scripts/processCommits.js
+++ scripts/processCommits.js
@@ -1,6 +1,8 @@
 const fs = require("fs/promises");
 const { exec } = require('child_process');
 
+const ISSUE_NUMBER_REGEX = /^ENG-(\d+)/
+
 async function fetchPR(commits){
     if(!commits?.length){
         return ""
@@ -42,19 +44,29 @@ async function fetchBranches(commits){
                         reject(error)
                     }
                     let branch = ""
+                    let issue
                     try{
                         const data = stdout ? stdout.split(" ") : ["", ""]
                         const fullPath = data.length > 1 ? data[1].trim() : ""
                         const path = fullPath.split("/")
                         const raw = path.length > 2 ? path[2] : fullPath
                         branch = raw.split("~")[0]
+                        const match = ISSUE_NUMBER_REGEX.exec(branch);
+                        if(match){
+                            issue = parseInt(match[1])
+                        }
                     }catch (e) {
                         reject(e)
+                        return
                     }
-                    resolve({
+                    const res = {
                         branch,
                         commit
-                    })
+                    }
+                    if(issue){
+                        res['issue'] = issue
+                    }
+                    resolve(res)
                 });
         })))
 }
@@ -63,19 +75,66 @@ async function process() {
     try {
         const data = await fs.readFile('./change_log/commits.txt', { encoding: 'utf8' });
         const lines = data.split("\n");
-        for(const line of lines){
-            console.log(line)
-        }
 
         const prs = await fetchPR(lines)
         console.log("------")
         console.log(prs)
 
-        const res = await fetchBranches(lines);
+        const branches = await fetchBranches(lines);
         console.log("******")
-        console.log(res)
+        console.log(branches)
+
+        const connectedBranches = branches.filter(branch => Boolean(branch.issue))
+
+        const commitsMap = {}
+
+        for(const b of connectedBranches){
+            commitsMap[b.commit] = `https://linear.app/orchid/issue/ENG-${b.issue}`
+        }
+
+        console.log(commitsMap)
+        const prMap = {}
+        for(const pr of prs){
+            if(pr.titles?.length !== 1){
+                continue
+            }
+            const title = pr.titles[0]
+            const notes = prMap[title] || []
+            if(!commitsMap[pr.commit]){
+                prMap[title] = notes
+                continue
+            }
+            if(notes.some(n => n === commitsMap[pr.commit])){
+                continue
+            }
+
+            notes.push(commitsMap[pr.commit])
+            prMap[title] = notes
+        }
+        console.log(prMap)
+
+        let changes = ""
+
+        Object.keys(prMap).forEach(pr =>{
+            changes += `# ${pr}\n`
+            if(prMap[pr].length){
+                changes +=`\nCompleted tasks:\n`
+                prMap[pr].forEach(issue =>{
+                    changes +=`- ${issue}\n`
+                })
+            }else{
+                changes +=`\nPR doesn't have related linear issues`
+            }
+            changes +='\n'
+        })
+
+        await fs.writeFile('./change_log/change_log.md', changes)
+
     } catch (err) {
         console.log(err);
+        for(const e of err.errors){
+            console.log(e)
+        }
     }
 }
 module.exports = process()
